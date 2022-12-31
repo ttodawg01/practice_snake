821d111b35b1adf5bf5a14aeff5e44b2a6aa9dd2
ttodawg01 <tonycampos1803@gmail.com>
2022-12-31 12:36:34 -0600

    the snake is now moving

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ game_ending = False
 
 snake_color = (255,255,255) 
 red = (255,0,0) #boxes we want to catch with the snake
+black = (0,0,0)
+
 
 x1 = 300
 y1 = 300
@@ -22,10 +24,30 @@ while not game_ending:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_ending = True
-        if event.type == pygame.KEYDOWN
-        pygame.draw.rect(dis,snake_color,[200,150,20,20]) #the [] determines the length and height of the snake moving
+        #keys to get our snake moving
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10 #going to the left with the left arrow key X axis
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10 #going to the right with the right arrrow key on the x axis
+                y1_change = 0
+            elif event.key == pygame.K_DOWN:
+                x1_change = 0
+                y1_change = 10 #press the bottom arrow key to go down on the Y axis
+            elif event.key == pygame.K_UP:
+                x1_change = 0
+                y1_change = -10 #press the up arrow key to go up the Y axis
+
+        x1 += x1_change #the reason we do this is because its based to do whatever the user presses to the box can change directions
+        y1 += y1_change
+        dis.fill(black) #make sure the background screen is black
+
+        pygame.draw.rect(dis, snake_color, [x1, y1, 20, 20])#the [] determines the length and height of the snake moving
         #dis represents all of this dislaying to the screen
         pygame.display.update()
+
+        clock.tick(30)
 
 
 pygame.quit()
